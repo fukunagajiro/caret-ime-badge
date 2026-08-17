d97b69aa48301785cc889484731f78c83eb5d8d2
Fukunaga Jiro <fukunagajiro@gmail.com>
2026-08-17 14:13:36 +0900

    fix: csc に /codepage:65001 を指定して UTF-8 判定を明示化
    BOM なしの .cs ファイルは OS の ANSI コードページに依存して解釈される。
    このマシンでは ACP が 65001 (UTF-8) のため今回は実害はないが、CP932 等
    の環境でビルドすると日本語コメントや文字列リテラル（Task 8 のバッジ
    文字など）が無警告で文字化けする。/codepage:65001 でこの依存を除去する。

--- build-test.cmd
+++ build-test.cmd
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 set CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
 set GAC=C:\Windows\Microsoft.NET\assembly\GAC_MSIL
 if not exist build mkdir build
-"%CSC%" /nologo /target:exe /platform:x64 /out:build\selftest.exe ^
+"%CSC%" /nologo /codepage:65001 /target:exe /platform:x64 /out:build\selftest.exe ^
   /r:System.dll /r:System.Core.dll /r:System.Drawing.dll /r:System.Windows.Forms.dll ^
   /r:Accessibility.dll ^
   /r:"%GAC%\UIAutomationClient\v4.0_4.0.0.0__31bf3856ad364e35\UIAutomationClient.dll" ^
